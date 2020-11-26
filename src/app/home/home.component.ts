53079799b7f3f015826705670af89d190b2430e3
zinedineBenkhider <zinedine.benkhider@gmail.com>
2020-11-26 02:07:45 +0100

    add typedJS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +11,15 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var options = {
+      strings: ['','Full-Stack', 'WEB','Mobile'],
+      typeSpeed: 120,
+      backSpeed: 100,
+      loop: true,
+    };
+    
+    var typed = new Typed('.typed', options);
+    typed.reset(true)
   }
 
 }
