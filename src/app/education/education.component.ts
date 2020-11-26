d69ec7038ef7eb3662f63e9af016d6661575767f
zinedineBenkhider <zinedine.benkhider@gmail.com>
2020-11-26 03:54:03 +0100

    languages & center of interest

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as awesom from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -12,6 +12,9 @@ export class EducationComponent implements OnInit {
   stageIsShown: boolean = false ;
   freelanceIsShown : boolean = false;
   alternanceIsShown:boolean = false;
+  planeIcon: any ;
+  gameIcon:any;
+  volleyBallIcon:any;
   detailOnClick(id:String) {
     if(id=="alternance_detail"){
       this.alternanceIsShown = !this.alternanceIsShown;
@@ -26,10 +29,12 @@ export class EducationComponent implements OnInit {
   }
  
   constructor() { 
-    
   }
 
   ngOnInit(): void {
+    this.planeIcon=awesom.faPlane;
+    this.gameIcon=awesom.faGamepad;
+    this.volleyBallIcon=awesom.faVolleyballBall;
   }
 
 }
